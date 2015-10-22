2a96e3577b0296999bc8ab2cb2548dfa5a525e10
Luke Imhoff <luke_imhoff@rapid7.com>
2015-10-22 16:30:58 -0700

    Fix dogma warnings in MSPP.Worker

--- lib/mspp/worker.ex
+++ lib/mspp/worker.ex
@@ -1,4 +1,8 @@
 defmodule MSPP.Worker do
+  @moduledoc """
+  Supervised worker that acts as ranch listener.
+  """
+
   def start_link do
     opts = [port: 8005]
     {:ok, _} = :ranch.start_listener(
