c79420f27da473d83d43597c8fdc1b9d83ceaca0
Luke Imhoff <luke_imhoff@rapid7.com>
2015-10-22 14:27:16 -0700

    mix new mspp --module MSPP --sup

---
+++ lib/mspp.ex
@@ -0,0 +1,19 @@
+defmodule MSPP do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # Define workers and child supervisors to be supervised
+      # worker(MSPP.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: MSPP.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ test/mspp_test.exs
@@ -0,0 +1,8 @@
+defmodule MSPPTest do
+  use ExUnit.Case
+  doctest MSPP
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
